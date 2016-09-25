fb7ca61ce4d7c2629aaf23e35a8f88dcb9406b7d
Anna Nedovba <anedovba@gmail.com>
2016-09-25 19:01:19 +0300

    Delete _all

--- _all/arrays_loops_tasks/22.php
+++
@@ -1,9 +0,0 @@
-<?php
-$n=$_POST['num1'];
-echo "пирамида из $n рядов"."<br>";
-$i=1;
-$row="";
-while ($i<=$n){
-    echo $row.="xx", '<br>';
-    $i++;
-}
--- _all/arrays_loops_tasks/9.php
+++
@@ -1,7 +0,0 @@
-<?php
-$num1=$_POST['num1'];
-$num2=$_POST['num2'];
-echo "числа от $num1 до $num2 <br>";
-for ($i=$num1;$i<$num2+1;$i++){
-    echo $i."<br>";
-}
